ea920f08726d4353e29a8150265f09a4e3e031bc
CarlJstn <carljustinedg@gmail.com>
2019-05-20 06:56:32 +0800

    Updates

---
+++ precache-manifest.1206a1ab8a2821b31138fced0c187755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b735044a398a46237b38980216a9c49b",
+    "url": "/reactjs-googlebooksapi/index.html"
+  },
+  {
+    "revision": "3ba6c8faaf29bea78453",
+    "url": "/reactjs-googlebooksapi/static/css/main.54720a1f.chunk.css"
+  },
+  {
+    "revision": "a1daa9737c2e95000380",
+    "url": "/reactjs-googlebooksapi/static/js/2.4e96293d.chunk.js"
+  },
+  {
+    "revision": "3ba6c8faaf29bea78453",
+    "url": "/reactjs-googlebooksapi/static/js/main.8a6d5a6e.chunk.js"
+  },
+  {
+    "revision": "a2db3eb8838880d6a662",
+    "url": "/reactjs-googlebooksapi/static/js/runtime~main.15b7b205.js"
+  },
+  {
+    "revision": "2f212d1f8d640a245cc3c17ce6ec94ed",
+    "url": "/reactjs-googlebooksapi/static/media/spinner.2f212d1f.gif"
+  }
+]);
